b41ee9b9786410c1771112f64bce6a83cbc2cca5
Chris Blaszczynski <christopher.blaszczynski@conduent.com>
2024-05-06 17:23:25 -0400

    Changed to use relative path for testing.

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -8,7 +8,7 @@ namespace api_process_runner_api.Helpers
 {
     public static class Constants
     {
-        public const string LocalFilePath = @"C:\temp\Data\";
+        public const string LocalFilePath = @"Data\";
         public const bool UseLocalFiles = true;
     }
 }
